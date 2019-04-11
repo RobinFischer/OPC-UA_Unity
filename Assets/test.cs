4860c903fa0faf98063a5f0b7b716868a4083d90
Asoma <asoma@users.noreply.github.com>
2019-04-11 14:49:20 +0200

    Update to Unity2018

---
+++ Assets/test.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OpcClientHda;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Client myClient = new Client();
+        myClient.Start();
+    }
+
+}
